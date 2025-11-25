50d79832c8e29f47c05d1e41992b8771958146fd
Avnoor Kaur <Avnoork2004@gmail.com>
2025-11-25 14:51:15 -0500

    Todo2 the DragAndDropBoxes composable

--- app/src/main/java/edu/farmingdale/draganddropanim_demo/MainActivity.kt
+++ app/src/main/java/edu/farmingdale/draganddropanim_demo/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : ComponentActivity() {
             DragAndDropAnim_DemoTheme {
 
                     DragAndDropBoxes()
+                //Todo2 the DragAndDropBoxes composable
 
 
             }
