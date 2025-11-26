1ad555eaf646961e01e7f6866d4729ef85faf12f
Avnoor Kaur <Avnoork2004@gmail.com>
2025-11-25 22:47:45 -0500

    Todo8 added a reset button so the rect/mailbox goes to the center of the (home icon) screen

--- app/src/main/java/edu/farmingdale/draganddropanim_demo/DragAndDropBoxes.kt
+++ app/src/main/java/edu/farmingdale/draganddropanim_demo/DragAndDropBoxes.kt
@@ -24,15 +24,18 @@ import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Face
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -162,6 +165,19 @@ fun DragAndDropBoxes(modifier: Modifier = Modifier) {
                     )
             )
         }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        //ToDo8 reset button
+        Button(
+            onClick = {
+                isPlaying = false
+            },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text("Reset Rectangle")
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
     }
 }
 
